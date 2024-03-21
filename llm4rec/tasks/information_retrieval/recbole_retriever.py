9d08553b5b05bf38b579f61cb7de9abb1d4ecb63
ainura-z <aynura_zakirova@mail.ru>
2024-03-21 16:59:30 +0300

    change of class

--- llm4rec/tasks/information_retrieval/recbole_retriever.py
+++ llm4rec/tasks/information_retrieval/recbole_retriever.py
@@ -1,11 +1,11 @@
 import torch
 import typing as tp
-from llm4rec.tasks.information_retrieval import RetrievalRecommender
+from llm4rec.tasks.information_retrieval import OpenAIRetrievalRecommender
 from recbole.model.abstract_recommender import SequentialRecommender
 from recbole.data.dataset import Dataset
 
 
-class RecBoleRetrievalRecommender(RetrievalRecommender, SequentialRecommender):
+class RecBoleRetrievalRecommender(OpenAIRetrievalRecommender, SequentialRecommender):
     """
     Adapter for RecBole of RetrievalRecommender model
 
@@ -13,6 +13,6 @@ class RecBoleRetrievalRecommender(RetrievalRecommender, SequentialRecommender):
         fake_fn (torch.nn.Module): Fake function to simplify adaptation to optimized RecBole models with .parameters()
     """
     def __init__(self, config: tp.Dict[str, tp.Any], dataset: Dataset, *args, **kwargs):
-        RetrievalRecommender.__init__(self, *args, **kwargs)
+        OpenAIRetrievalRecommender.__init__(self, *args, **kwargs)
         SequentialRecommender.__init__(self, config=config, dataset=dataset)
         self.fake_fn = torch.nn.Linear(1, 1)
