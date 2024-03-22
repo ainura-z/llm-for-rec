1584761a66f8810bfd61bc750fe62a1f933099b8
ainura-z <aynura_zakirova@mail.ru>
2024-03-22 13:49:04 +0300

    adding recommender import

--- llm4rec/tasks/__init__.py
+++ llm4rec/tasks/__init__.py
@@ -1,3 +1,4 @@
+from llm4rec.tasks.base_recommender import Recommender
 from llm4rec.tasks.information_retrieval.openai_retriever import (
     OpenAIRetrievalRecommender,
 )
@@ -5,6 +6,7 @@ from llm4rec.tasks.information_retrieval.recbole_retriever import RecBoleRetriev
 from llm4rec.tasks.ranker.openai_ranker import OpenAIRanker
 
 __all__ = [
+    "Recommender",
     "OpenAIRetrievalRecommender",
     "RecBoleRetrievalRecommender",
     "OpenAIRanker"
