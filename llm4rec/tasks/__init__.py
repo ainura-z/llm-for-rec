999faebac7f9f346e8823119fd1472439e94749b
cmonkl <cmon86559@gmail.com>
2024-03-27 12:24:17 +0300

    Changed openai retriever to general retriever

--- llm4rec/tasks/__init__.py
+++ llm4rec/tasks/__init__.py
@@ -1,11 +1,11 @@
-from llm4rec.tasks.information_retrieval.openai_retriever import (
-    OpenAIRetrievalRecommender,
+from llm4rec.tasks.information_retrieval.general_retriever import (
+    RetrievalRecommender,
 )
 from llm4rec.tasks.information_retrieval.recbole_retriever import RecBoleRetrievalRecommender
 from llm4rec.tasks.ranker.openai_ranker import OpenAIRanker
 
 __all__ = [
-    "OpenAIRetrievalRecommender",
+    "RetrievalRecommender",
     "RecBoleRetrievalRecommender",
     "OpenAIRanker"
 ]
--- llm4rec/tasks/information_retrieval/recbole_retriever.py
+++ llm4rec/tasks/information_retrieval/recbole_retriever.py
@@ -1,11 +1,11 @@
 import torch
 import typing as tp
-from llm4rec.tasks.information_retrieval import OpenAIRetrievalRecommender
+from llm4rec.tasks.information_retrieval import RetrievalRecommender
 from recbole.model.abstract_recommender import SequentialRecommender
 from recbole.data.dataset import Dataset
 
 
-class RecBoleRetrievalRecommender(OpenAIRetrievalRecommender, SequentialRecommender):
+class RecBoleRetrievalRecommender(RetrievalRecommender, SequentialRecommender):
     """
     Adapter for RecBole of RetrievalRecommender model
 
@@ -13,6 +13,6 @@ class RecBoleRetrievalRecommender(OpenAIRetrievalRecommender, SequentialRecommen
         fake_fn (torch.nn.Module): Fake function to simplify adaptation to optimized RecBole models with .parameters()
     """
     def __init__(self, config: tp.Dict[str, tp.Any], dataset: Dataset, *args, **kwargs):
-        OpenAIRetrievalRecommender.__init__(self, *args, **kwargs)
+        RetrievalRecommender.__init__(self, *args, **kwargs)
         SequentialRecommender.__init__(self, config=config, dataset=dataset)
         self.fake_fn = torch.nn.Linear(1, 1)
