4bae1da528ea39d11bbe571e455f9f4b31134b06
ainura-z <aynura_zakirova@mail.ru>
2024-04-03 14:24:56 +0000

    importing model for explanation

--- llm4rec/tasks/__init__.py
+++ llm4rec/tasks/__init__.py
@@ -3,9 +3,11 @@ from llm4rec.tasks.information_retrieval.general_retriever import (
 )
 from llm4rec.tasks.information_retrieval.recbole_retriever import RecBoleRetrievalRecommender
 from llm4rec.tasks.ranker.openai_ranker import OpenAIRanker
+from llm4rec.tasks.explanation.explanation import ExplainableRecommender
 
 __all__ = [
     "RetrievalRecommender",
     "RecBoleRetrievalRecommender",
-    "OpenAIRanker"
+    "OpenAIRanker",
+    "ExplainableRecommender"
 ]
