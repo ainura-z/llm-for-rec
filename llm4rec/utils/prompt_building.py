fc19ebc42f9abd330c6bbbb7b0ec2d1e68c2c554
ainura-z <aynura_zakirova@mail.ru>
2024-05-07 21:37:57 +0300

    Adding prompt formatting

---
+++ llm4rec/utils/prompt_building.py
@@ -0,0 +1,13 @@
+import typing as tp
+
+import pandas as pd
+
+
+def prepare_input_per_users(
+    prompt: str,
+    user_profile: str,
+    prev_interactions: tp.Dict[str, str],
+    top_k: int
+) -> str:
+    
+    return prompt.format(user_profile=user_profile, item_ids_with_meta=prev_interactions, top_k=top_k)
