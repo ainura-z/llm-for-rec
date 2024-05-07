30f8d8d4735d9f30ba450cedaf50c81c6e9e6874
Ainura <ainura@MacBook-Pro-Ainura.local>
2024-05-07 19:29:10 +0300

    Adding importing of create retrieval and ranker tools

--- llm4rec/tools/__init__.py
+++ llm4rec/tools/__init__.py
@@ -1,5 +1,9 @@
 from llm4rec.tools.create_tool import create_tool
+from llm4rec.tools.retrieval import create_retrieval_tool
+from llm4rec.tools.ranker import create_ranking_tool
 
 __all__ = [
     "create_tool",
+    "create_retrieval_tool",
+    "create_ranking_tool",
 ]
