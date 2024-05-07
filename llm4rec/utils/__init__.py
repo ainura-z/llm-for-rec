0338d1d0dd8e6917b167aaa727b469a41e6aab5c
ainura-z <aynura_zakirova@mail.ru>
2024-05-07 21:39:47 +0300

    Adding import

--- llm4rec/utils/__init__.py
+++ llm4rec/utils/__init__.py
@@ -1,5 +1,7 @@
 from llm4rec.utils.file_embeddings import EmbeddingsFromFile
+from llm4rec.utils.prompt_building import prepare_input_per_users
 
 __all__ = [
-    "EmbeddingsFromFile"
+    "EmbeddingsFromFile",
+    "prepare_input_per_users"
 ]
