d33cecaae3fe791d5ae36a79ba152da2d91ac645
ainura-z <aynura_zakirova@mail.ru>
2024-05-17 18:27:03 +0300

    Adding default prompts for planning and replanning

--- llm4rec/agents/base_agent.py
+++ llm4rec/agents/base_agent.py
@@ -10,13 +10,42 @@ class AgentBase(metaclass=ABCMeta):
     Warning: This class should not be used directly.
     Use derived classes instead.
     """
-    default_prompt_for_agent_planning: str = None # TODO
-    default_prompt_for_agent_replanning: str = None # TODO
-    default_prompt_for_agent_reflexion: str = None # TODO
-    default_prompt_for_agent_executor: str = "You are very powerful assistant for recommedation system, which uses information based on historical user data. "\
-                                            + "You have access to the following tools: {tool_description_with_args}. " \
-                                            + "Please use the tools to provide the best recommendations for the user."       
-  
+    default_prompt_for_agent_planning: str = """
+                                                For the given objective, come up with a simple step by step plan.
+                                                Here are the tools could be used: 
+                                                retrieval_recommender: tool for finding similar candidate items based on previous interactions of the user
+                                                item_dataset_pair_info: tool for mapping id of the item to its attributes, used before Ranker
+                                                ranker_recommender: tool for ranking candidates items
+
+                                                First you need to think whether to use tools. If no, give the answer.
+
+                                                Objective: {objective}
+                                                Plan should be in the following form:
+                                                {{
+                                                    "steps": tp.List[str] = Field(description="different steps to follow, should be in sorted order")
+                                                }}
+                                                Just give the Plan WITHOUT calling the functions.
+                                            """
+
+    default_prompt_for_agent_replanning: str = """
+                                                There is a recommendation agent.
+                                                The agent could use several tools to deal with the objective. Here are the description of those tools: {tools_description}
+                                                When giving judgement, you should consider whether the tool using is reasonable? 
+                                                For example, ranker tool cannot be used before retrieval tool. And as retrieval tool returns only ids, item_dataset_pair_info should be used before ranker tool.
+                                                But the agent could use only retrieval tool, which is also fine.
+
+                                                If the plan is reasonable, you should ONLY output "Yes". 
+                                                If the plan is not reasonable, you should give "No. The response is not good because ...".
+
+                                                The plan is the following: {plan}
+                                            """
+    default_prompt_for_agent_reflection: str = None # TODO
+    default_prompt_for_agent_executor: str = """
+                                                You are very powerful assistant for recommedation system, which uses information based on historical user data.
+                                                You have access to the following tools: {tool_description_with_args}.
+                                                Please use the tools to provide the best recommendations for the user.       
+                                            """
+    
     default_prompt_for_user: str = (
         "Task: User {user_profile}. This User has previous interactions with these items: {item_ids_with_meta}. Please give {top_k} candidate items recommendations for this user considering his preferences."
     )
@@ -47,7 +76,7 @@ class AgentBase(metaclass=ABCMeta):
         self.prompt_for_agent_executor = prompt_for_agent_executor or self.default_prompt_for_agent_executor
         self.prompt_for_agent_planning = prompt_for_agent_planning or self.default_prompt_for_agent_planning
         self.prompt_for_agent_replanning = prompt_for_agent_replanning or self.default_prompt_for_agent_replanning
-        self.prompt_for_agent_reflexion = prompt_for_agent_reflection or self.default_prompt_for_agent_reflexion
+        self.prompt_for_agent_reflexion = prompt_for_agent_reflection or self.default_prompt_for_agent_reflection
         
         self.llm_executor = llm_executor
         self.agent_executor = self._create_agent_executor(*args, **kwargs)
