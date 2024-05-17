21c7343a04a5c9bb90bdfb339131d8c1f1605242
ainura-z <aynura_zakirova@mail.ru>
2024-05-17 18:18:13 +0300

    Import of Plan-and_execute agent

--- llm4rec/agents/__init__.py
+++ llm4rec/agents/__init__.py
@@ -1,7 +1,9 @@
 from llm4rec.agents.base_agent import AgentBase
 from llm4rec.agents.agent.simple_agent import SimpleAgent
+from llm4rec.agents.agent.plan_execute_agent import PlanExecuteAgent
 
 __all__ = [
     "AgentBase",
-    "SimpleAgent"
+    "SimpleAgent",
+    "PlanExecuteAgent",
 ]
