5ee52477be95bbdfccf1ee23b1038a1c54f06a0c
ainura-z <aynura_zakirova@mail.ru>
2024-05-22 21:28:10 +0300

    adding prompts related to tools

---
+++ llm4rec/prompts/tools.py
@@ -0,0 +1,32 @@
+RETRIEVAL_TOOL_DESC = """
+                    Tool for finding similar candidate items based on previous interactions of the user.
+                    This tool should be used as a first stage of recommendation pipeline.
+                    """
+ITEM_DATASET_PAIR_INFO_DESC = """
+                            Tool for mapping id of the item to its attributes.
+                            This is an auxiliary tool, which is used before Ranker.
+                            """
+RANKER_DESC = """
+            Tool is used to rank candidate items.
+            This tool should be used after retrieval tools.
+            """
+
+_TOOL_DESC = {
+    "RETRIEVAL_TOOL_DESC": RETRIEVAL_TOOL_DESC,
+    "RANKER_DESC": RANKER_DESC,
+    "ITEM_DATASET_PAIR_INFO_DESC": ITEM_DATASET_PAIR_INFO_DESC
+}
+
+OVERALL_TOOL_DESC = """
+- "retrieval_recommender": {RETRIEVAL_TOOL_DESC}
+- "ranker_recommender": {RANKER_DESC}
+- "item_dataset_pair_info": {ITEM_DATASET_PAIR_INFO_DESC}
+""".format(
+    **_TOOL_DESC
+)
+
+RULES = """
+        - Retrieval tool should be first invoked.
+        - Ranker tool should be invoked after retrieval tool.
+        - If Ranker will be invoked, then item_dataset_pair_info tool should be invoked before ranker tool as retrieval tool returns only ids.
+        """
