e3b47d88c18189a06b7942f65c92227f93c7ef10
ainura-z <aynura_zakirova@mail.ru>
2024-05-22 22:51:37 +0300

    changing retrieval tool

--- llm4rec/prompts/tools.py
+++ llm4rec/prompts/tools.py
@@ -1,21 +1,28 @@
 RETRIEVAL_TOOL_DESC = """
                     Tool for finding similar candidate items based on previous interactions of the user.
                     This tool should be used as a first stage of recommendation pipeline.
+                    This tool has 3 parameters: user_profile, which is just a string, previous_interactions, which is dictionary of item id and attribute, and top_k. 
                     """
+
+
 ITEM_DATASET_PAIR_INFO_DESC = """
                             Tool for mapping id of the item to its attributes.
                             This is an auxiliary tool, which is used before Ranker.
                             """
+
+
 RANKER_DESC = """
             Tool is used to rank candidate items.
             This tool should be used after retrieval tools.
             """
+
 
 _TOOL_DESC = {
     "RETRIEVAL_TOOL_DESC": RETRIEVAL_TOOL_DESC,
     "RANKER_DESC": RANKER_DESC,
     "ITEM_DATASET_PAIR_INFO_DESC": ITEM_DATASET_PAIR_INFO_DESC
 }
+
 
 OVERALL_TOOL_DESC = """
 - "retrieval_recommender": {RETRIEVAL_TOOL_DESC}
@@ -24,6 +31,7 @@ OVERALL_TOOL_DESC = """
 """.format(
     **_TOOL_DESC
 )
+
 
 RULES = """
         - Retrieval tool should be first invoked.
