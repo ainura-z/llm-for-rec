13aaa5728472220879c80786e550925e8b3f6ac3
cmonkl <cmon86559@gmail.com>
2024-05-24 19:36:33 +0000

    Added documentation

--- llm4rec/tasks/augmentation/item_augmentation.py
+++ llm4rec/tasks/augmentation/item_augmentation.py
@@ -1,16 +1,39 @@
 import typing as tp
 from llm4rec.memory.base_memory import BaseMemory
 
+
 class ItemAugmentation:
-    def __init__(self, item_memory: BaseMemory):
+    """
+    A task that augments items with new data from external knowledge
+    """
+
+    def __init__(self, item_memory: BaseMemory) -> None:
+        """
+        Initializes ItemAugmentation
+
+        Args:
+            item_memory: ItemMemory storage with additional information about each item
+        """
         super().__init__()
         self.memory = item_memory
 
-    def transform(self, prev_interactions: tp.List[str]):
+    def transform(self, prev_interactions: tp.List[str]) -> tp.Dict[str, str]:
+        """
+        For each item id in prev_interactions extract additional information
+        from memory.
+
+        Args:
+            prev_interactions (List[str]): List of item ids
+
+        Returns:
+            dict[str, str]: dictionary mapping from item ids to corresponding text data
+        """
         prev_interactions_info = []
 
         for item_id in prev_interactions:
             additional_text_data = self.memory.retrieve(item_id)
             prev_interactions_info.append(additional_text_data)
-
-        return dict(zip(prev_interactions, prev_interactions_info))
+        augmented_prev_interactions = dict(
+            zip(prev_interactions, prev_interactions_info)
+        )
+        return augmented_prev_interactions
--- llm4rec/tasks/augmentation/user_augmentation.py
+++ llm4rec/tasks/augmentation/user_augmentation.py
@@ -1,11 +1,25 @@
 import typing as tp
 from llm4rec.memory.base_memory import BaseMemory
 
+
 class UserAugmentation:
+    """
+    Task that adds user profile info
+    """
+
     def __init__(self, user_memory: BaseMemory):
+        """
+        Initializes UserAugmentation
+
+        Args:
+            user_memory: UserMemory instance with all available information about user
+        """
         super().__init__()
         self.memory = user_memory
 
     def transform(self, user_token_id: str):
+        """
+        Generates user profile
+        """
         user_profile = self.memory.construct_user_profile(user_token_id)
         return user_profile
