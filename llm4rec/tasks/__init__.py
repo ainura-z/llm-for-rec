b76fdd7eb7e88be7aa04cafa0d60440cbd7c2898
ainura-z <aynura_zakirova@mail.ru>
2024-06-02 13:32:57 +0300

    fix name of folder

--- llm4rec/tasks/__init__.py
+++ llm4rec/tasks/__init__.py
@@ -3,7 +3,7 @@ from llm4rec.tasks.information_retrieval.general_retriever import (
 )
 from llm4rec.tasks.augmentation.item_augmentation import ItemAugmentation
 from llm4rec.tasks.augmentation.user_augmentation import UserAugmentation
-from llm4rec.tasks.ranker.general_ranker import RankerRecommender
+from llm4rec.tasks.ranking.general_ranker import RankerRecommender
 from llm4rec.tasks.recbole_models.model_wrappers import GeneralRecBoleModelWrapper, SequentialRecBoleModelWrapper
 from llm4rec.tasks.explanation.explanation import ExplainableRecommender
 
